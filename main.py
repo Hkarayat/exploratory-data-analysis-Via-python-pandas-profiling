7169090e14c9c0b7ebc0cbc83bc2feb0cf1e85d0
hkarayat4 <hkarayat4@gmail.com>
2022-06-28 11:40:01 +0530

    Extraploratory

---
+++ main.py
@@ -0,0 +1,15 @@
+from profile import Profile
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+
+
+# importing the data from csv
+df = pd.read_csv("winemag-data-130k-v2.csv")
+
+# generating the report
+
+report = ProfileReport(df)
+report.to_file(output_file = "report.html" )
+
